5c4c046d88ea127c20faab5c2cfe193195e12ecf
Mateus Silva <mateusdasilvavilaca@gmail.com>
2025-06-02 18:06:17 -0300

    inicio exercicio da lâmpada

---
+++ lampada/script.js
@@ -0,0 +1,18 @@
+//Seleciona elementos:
+let img = document.getElementById('imagem')
+
+function entrou() {
+    img.src = "imagens/ligada.jpg"
+}
+function ligar() {
+    img.src = "imagens/ligada.jpg"
+}
+function desligada() {
+    img.src = "imagens/desligada.jpg"
+}
+function quebrada() {
+    img.src = "imagens/quebrada.jpg"
+}
+function saiu() {
+    img.src = "imagens/desligada.jpg"
+}
